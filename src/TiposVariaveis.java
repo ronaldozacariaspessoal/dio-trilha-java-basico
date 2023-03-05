83c2ab907e8c2f5e3bda376b9c6bc8949089e547
ronaldozacariaspessoal <ronaldozacarias.pessoal@gmail.com>
2023-03-05 01:44:57 -0300

    Exemplos

--- src/TiposVariaveis.java
+++ src/TiposVariaveis.java
@@ -1,5 +1,23 @@
 public class TiposVariaveis {
     public static void main(String [] args) {
         System.out.println("Hello World! ");
+        byte idade = 123;
+        short ano = 2021;
+        int cep = 21070333; // se começar com zero, talvez tenha que ser outro tipo
+        long cpf = 98765432109L; // se começar com zero, talvez tenha que ser outro tipo
+        float pi = 3.14F;
+        double salario = 1275.33;
+        final double VALOR_DE_PI = 3.14;
+        System.out.println("byte: "+idade);
+        System.out.println("short: "+ano);
+        System.out.println("int : "+cep);
+        System.out.println("long: "+cpf);
+        System.out.println("float: "+pi);
+        System.out.println("double: "+salario);
+        System.out.println("final double: "+VALOR_DE_PI);
+
+
+
+
     }
 }
